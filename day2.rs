5a7aa2b46f7f58772c6a6d0f0c2df7f7385457ad
miiiila <mila@shrug.cz>
2018-12-02 11:43:28 +0000

    Day 2

---
+++ day2.rs
@@ -0,0 +1,64 @@
+fn main() {
+let mut input = ["uqcipadzntnheslgvjjozmkfyr","uqcipadzwtnhexlzvxjobmkfkr","cqcipadpwtnheslgyxjobmkfyr","ubnipadzwtnheslgvxjobmkfyw","uqcisadzwtnheslgvxjfbmkfor","uqcisaezwtnheslgvxkobmkfyr","uqcguadzwtnheslgvxjobmkfir","uqcipadzmtnhesldvxdobmkfyr","uqcipadzwtzheslgdxjtbmkfyr","uquipadzwtcheslgvxjobmkfbr","uqctpadzwtnhesjbvxjobmkfyr","ueciparzwtnheslgvxjobmkfyx","uqcipadzwtnhessgvxjkbmkfkr","uqcipamzwtnheslgvxioamkfyr","uciizadzwtnheslgvxjobmkfyr","uqcieadzwtnhesfgvxeobmkfyr","fqcipadzwtnreslgvkjobmkfyr","uqcipadzrtnherlgvxjobmklyr","uqcypadzwtnheslgvxjobmkxfr","uqcipadzwtnhemlgvxjobmvfur","uwciuadzwwnheslgvxjobmkfyr","uqcipadzwtnhcscgvxjobmkuyr","upripadzwtnheslovxjobmkfyr","uqcipadzltnheslgvxjobmkftc","uqcipadzwtnheslgvgjobmifsr","uqoipadzwtnheslgvxjosmkfkr","uqcipadzwtbhesrqvxjobmkfyr","uqcipadzwtnheslpvxjobmhfyx","uhcinadzwtnheslgvxjybmkfyr","uqcipadzwtnhhslgvxjabmkbyr","uecipadzwtnheslgvxjobqyfyr","uqcipadfwtnheslwvxjobgkfyr","uqcipadzvtnheshgvxzobmkfyr","fqcipadzwtcheslgvxjobmkfyt","uecipadzwtnheslgpxjbbmkfyr","uqclpadzwtnheslgvnjobukfyr","qqciprdzetnheslgvxjobmkfyr","uqcipahpwtnheslgvxjtbmkfyr","uqcidadzwtnhesljvxyobmkfyr","uqciradswtnqeslgvxjobmkfyr","uqcipadzwtrhmslgvxjobmkfyf","urcipadzjtnheslgvxfobmkfyr","uqcipadzwznheslgvxjobmkfcv","uqcipadowtnheslgyxjobmkfym","uqcigadzwtnheslgvxjoomkmyr","uqjipafzwtnheslgvejobmkfyr","uqcioadzwtnhhslgvxzobmkfyr","uqcgpadkwtnheslgvxjobhkfyr","ufciiadewtnheslgvxjobmkfyr","uqoipadzwtnheslgvxjllmkfyr","uqcipadzutnheslgwxxobmkfyr","uqcipadzwtlheslgaxjobmkfwr","uqcbpadzutnheslgvxjbbmkfyr","uucipadzwvnhesngvxjobmkfyr","uqcifadzwtnceslgvxjoumkfyr","ujcipadzwteheslgvxjobmkfyj","uqcipadzwtnheslqvxjobmkuyp","uqcipadzwtnheslgvxjoxmkxyw","uqcipaduwtnheslgvujmbmkfyr","uicipadnwtnheslgvxjobmbfyr","uqcipadzwteheslgvxjobbmfyr","uqcipadzwgnneslgvxjobmklyr","uqcipadzxtnhwslgvbjobmkfyr","uqcipaxwwtnheslxvxjobmkfyr","uocipadzwtnheslgvxjobqdfyr","uqciaauzwtnheslgtxjobmkfyr","uncipagzwtnkeslgvxjobmkfyr","uqcipadzwtnhehlgvxjohdkfyr","uqcipadzwtnheslgvxjobmspyz","uccipadzwtnhvsltvxjobmkfyr","uacipagzwtnheslgvxjoqmkfyr","tqcipaduwtnheslgvxjobmmfyr","uqcipadzwtnheslgvxqebmifyr","uecipadthtnheslgvxjobmkfyr","uocipadzwtnhdslgvkjobmkfyr","uqcipadtwtnheslgvxhobmufyr","uqkipadzwtnleslgtxjobmkfyr","uqcipadzjunheslgvxjobmnfyr","ubcipadzwtvheslgvxjobmkfyf","uqcipadzwpfheslgvxjsbmkfyr","uocipadzwtndeslgvxjobmmfyr","uqcipadzwtnheslgtxjobhkfyq","uqcipadzwtrheslgvxjobmyfya","uqcipadzwtvheslgvxjolgkfyr","uqcipidzwtaheslgvxjobmkfxr","uyzixadzwtnheslgvxjobmkfyr","uqyihadzwtnhedlgvxjobmkfyr","uqcipadzwtnhesltvejobqkfyr","uqciptdzwtnheslgyxlobmkfyr","uqcipzdzwtnhzslgvxjosmkfyr","uqcipadzwtnbeslgexjobmkfvr","uqcipadzwtnheslcwxjobmkkyr","uqcapadzwcnheslgvxjolmkfyr","uqcjpadzwtnhejlgvxjxbmkfyr","uqcipadwwtxweslgvxjobmkfyr","uqmipadzwtnhezlgvxjobmkyyr","uqcipubzwtnpeslgvxjobmkfyr","uecvpadzwtnheslgvxjocmkfyr","uqcipadzwfnheslgvxjibmkdyr","uqcipadzwtnheslgvxvfbykfyr","uqcipadzwtnheslgvgjoimkfyt","dqcqpaqzwtnheslgvxjobmkfyr","uqcipbdzwtnheslgvxjobmkghr","jqcipadzwtnheslgvxjgbmkzyr","uqcipadzwtnheslgvxqkqmkfyr","uqcipadzptnheslgvxjxbokfyr","uucijadzwtwheslgvxjobmkfyr","uccfpadzwtnheslgvxjobpkfyr","uqcipadzwtnheslgvxjobakeyq","uqcipadzwtnheolgvxqobjkfyr","imiipadzwtnheslgvxjobmkfyr","uqcehadzwtnheslgvxjobmkuyr","uqcipadzztnheslgvxjorokfyr","rqcixadzwtnheelgvxjobmkfyr","uqcipadzwtzheslgvxjodmkfyi","uqcipaezwtnwuslgvxjobmkfyr","uqcipadzwtnheslggxjobjkfyq","uqcipadzwkghesagvxjobmkfyr","uqcypqdzwtnheslgvxjobakfyr","iqcipadzwtnhezltvxjobmkfyr","uxcimadzwtnheslgvxjobmxfyr","uqcipaizwtvhwslgvxjobmkfyr","uqcipafzwtnheslgvxjpbmkfym","uqcipadzwinheslgvxlobmpfyr","uqcupadzwtnheslkvxmobmkfyr","uqcapadzwtnhesrgvxjobmkfsr","urcipafzwtnheslgvxjobmkfur","uqcipaczwtnheslgvbjobmknyr","uqcizadzztgheslgvxjobmkfyr","uqcipfdzwtnhesxgvxjobmkfyw","uqcipbdzwtnhyslgvxjobmcfyr","uqcipadzwanhezlgvxjobmkfwr","uvcipadzwtnheslgvxjbkmkfyr","uqcipajzwtnseslgvxjobmkfyq","uqcipvdzwtnheslgvmlobmkfyr","uqcipadzdgnheslgmxjobmkfyr","uqcipddzwtnhestgvpjobmkfyr","umcipadzwtdheslgvxjzbmkfyr","uqciuwdzwtnheslgvxjobmkflr","uqcipadzwtnheslgsxabbmkfyr","uceipadzwtnheslgvxjobgkfyr","mqcipadzwtnhesrgvxjobmjfyr","aqcipadvwtnheslgvxjobmkryr","uqsipadzwtnofslgvxjobmkfyr","uqcixadzwtfheslgvxjzbmkfyr","uqcipadnwfnheslgvxjohmkfyr","uqcivadzwtnheslfvxjobmkfyz","uqciprdzwtnheslgvxjobmkjir","uqcipadhbtnheslgvxjoxmkfyr","fqcipadzwtnhesfgvxjobmkfye","uqoipqdzwtnheqlgvxjobmkfyr","uqcipadzwtnhesltvxmobmkzyr","uqcipadzwtnhebqgvsjobmkfyr","uqcipadzwtnheslglxjobmfbyr","gqcipadzwtgheslgvxjobwkfyr","uqcipadzwtnheslgfxjzbmlfyr","ujcnpadzwtnheslrvxjobmkfyr","ujcivadzwtnheglgvxjobmkfyr","uqcitadzwgnheslgvxjofmkfyr","uqcipahzatnhmslgvxjobmkfyr","uqzipaizwtnheslgvujobmkfyr","uqcipadzltnheylgvnjobmkfyr","uqcidadzwtnhwsljvxyobmkfyr","uqcipadzwtihetlgvxjobhkfyr","oqcipabzwtnheslgvfjobmkfyr","uqcipadzwtnveslgvxjobzkfzr","uqcipadzwtjheslgqxjobmlfyr","uqcnpadzztnheslgvxjobmkoyr","uqciuadzwonheslgvxjobmkfyz","tqcipadzwtnheslgvxaobmqfyr","uqcipadtwtnhqslgvxjobmkeyr","uqcipadzwbnheslgvajobmsfyr","ubcopadzwtnhgslgvxjobmkfyr","uqcipydzwtwheslgvxjobakfyr","cqbijadzwtnheslgvxjobmkfyr","uscipadowtnheslgvxjobmkfcr","uqcipadzwtgheslnvxjobskfyr","uqcipzdzwtnzeslgkxjobmkfyr","uqcipawzwtnhrslgbxjobmkfyr","uqcipadzatchyslgvxjobmkfyr","uqcipadzotnpeslgvxjobmjfyr","uqcipagzwtnheslgvxjobmvfyt","uqcipadzwhnheslgvxyobmkfyo","uqcipadzwtnheslgmqjobmkfyc","uqcupadzwgnheslgvcjobmkfyr","uqcipabzwbnheslgvxjobmkwyr","uqciiadzwtnheslgvxjobmkfmz","uqkipauzwtnheslgvxjjbmkfyr","uqcipidzetnheslgvxjobmkfyi","uqcipadzwtnheslgqxjokmkfmr","uqcipadzqtnhesllvxjobmkfyk","uqccpadzwtnheslgmxsobmkfyr","uqcipadzwteheslgvljfbmkfyr","uqcipadxwinheslgaxjobmkfyr","uqcipadzwtnheslhvxyobmkfjr","aqcipadzwnnheslgvxjqbmkfyr","uvcipadzwtnheszgvxjobmkfyg","uqcipahzmtnheslgvxjobmkfir","ukcipadzbtnheslgvxjobmkfyb","uqcipadzwtnhemlgvqjobmkfpr","uqcipadzwtnheslgvmeobmkfpr","uqciphdrwtnheslgvxjobmkfyw","uqcipadzwtnheslevxqobzkfyr","uqcipadzwknzeslgvxnobmkfyr","wqcipadzwjnheslgvxjobbkfyr","uqcipadzwtdheslgvmjobmkjyr","uqvipadzwtnhextgvxjobmkfyr","uqhipadzwtnheslwvxjzbmkfyr","uqcipadzwtnherlgsxjobmksyr","uqcipadzwtnhesqgvxjotmvfyr","udcipadzwtnhekwgvxjobmkfyr","uqcjprdzwtnheslgvxjobmkfpr","uqcipadzatnheclgvqjobmkfyr","uqcbpadzctnheslqvxjobmkfyr","uqcipadzqtnhesluvxjobrkfyr","uqcipadzwtnhcslgvxjoomwfyr","uqcppadzwxnheslgwxjobmkfyr","uqcipadcwtnheslrvxjdbmkfyr","ukcipadzwtnhhslgvxjobmkgyr","uqckpadzwtnheslgvxjokmkiyr","uqcspadzwtjheslgvxjobmkfjr","uqcipadpwtnhsslgvxjobmkfyu","uqcepadzwtnheilgvbjobmkfyr","jqcipadiwtnheslgvxjobmkjyr","uqcipadzrtnseslgqxjobmkfyr","sqmipadzwtnhewlgvxjobmkfyr","uqcieadzhtnheslgvgjobmkfyr","uqcipadzwkwhewlgvxjobmkfyr","uqcipadzwtzheslgvxjpbqkfyr","uzcipadzjtnheslgvxjobmlfyr","uqcipadzwtnheslnvxjobmkfee","uqciyanzwtnheslgvxjoimkfyr","uqcipadqwtnheswghxjobmkfyr","uycipadzwtnheslovxjobmofyr","uqcipadzwtnheslgvxcozmxfyr","uqmipadzwtnxezlgvxjobmkfyr","uqcipadzftnheslgvxjotmkffr","aqcipaizwtnhesagvxjobmkfyr","uqcipcdzwtnheslgoajobmkfyr","uqcypadgwtnhesbgvxjobmkfyr","uqcipcdzwtnheslgvxjebmkfyb","uhcvpadzwtnheslgvxjobzkfyr","uqcipadzwtnpesagvxmobmkfyr","uqcipadzwtnidslgvxjobmkfor","uqcipadkwtnhesigvxjzbmkfyr","uqcypadlwtnheslsvxjobmkfyr","qqcipadzwtnheswgvxjobmkoyr","uqcipadzwtnheslgvxjhbmmcyr","uqcipadzwtnhesogvxjormkfmr","uqcipadzwtnhetcgvxgobmkfyr"];
+
+// Part 1
+	let mut two = 0; 
+	let mut three = 0;
+ 
+	for word in input.iter() {
+		let res = count_chars(word.to_string());
+		if res.0 {
+			two = two + 1;
+		}
+		if res.1 {
+			three = three + 1;
+		}
+	}
+
+	println!("{}", two * three);
+
+	fn count_chars(word: String) -> (bool, bool) {
+		// intentionally without sorting word
+		let mut count: [u8;26] = [0;26];
+		let mut two = 0;
+		let mut three = 0;
+		for c in word.bytes() {
+			let index = (c-b'a') as usize;
+			count[index] = count[index]+1;
+			if count[index] == 2 {
+				two = two + 1;
+			} 
+			if count[index] == 3 {
+				two = two - 1;
+				three = three + 1;
+			} 
+		}
+
+		return (two > 0, three > 0);
+	}
+
+// Part2
+	input.sort();
+	let mut i = 0;
+	let mut j = 1;
+	while j < input.len() {
+		let mut diff = 0;
+		let mut s = String::from(input[i]);
+		let mut t = String::from(input[j]);
+		while s.len() > 0 {
+			if s.pop() != t.pop() {
+				diff = diff + 1;
+			}	
+			if diff > 1 {
+				break;
+			}
+		}
+		if diff ==  1 {
+			println!("{}", input[i]);
+			println!("{}", input[j]);
+			break;
+		}
+	i = i + 1;
+	j = j + 1;
+	}
+}
